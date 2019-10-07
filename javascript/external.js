651a5e1c3d486c737ebf9a132da69e557e66ee7e
Dunlap <srd1@txstate.edu>
2019-10-07 16:15:50 -0500

    Made most modifications for lab 5

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,21 @@
+
+
+var webmaps =
+[
+
+ ["Zillow" ,"https://www.Zillow.com" , "The map allows the user to utilize multiple search queries to find the home they are looking at. " ],
+
+["The Hiking Project" , "https://www.hikingproject.com/" , "The primary purpose of the application it to highlight hiking trail systems and provide user data on trails." ]
+];
+
+
+
+
+
+
+
+
+
 function welcome ()
 {
 let a = "Please enter your name.";
@@ -10,4 +28,30 @@ message = "<h1>Hello, welcome to my webpage, " + user_name +"!</h1>"
 
 return message
 
+} ;
+
+
+
+
+
+function webmap_table ()
+{
+document.write ("<table width = 100%>");
+for (var row=0; row < webmaps.length; row++)
+{
+document.write("<tr>");
+for (var column=0; column < webmaps[0].length; column++);
+{
+  if (column < 2){
+    document.write ("<td>") + webmaps [rows][columns] + "</td>");
+  }
+    else  {
+    document.write("</tr>");
+    document.write("<tr>");
+    document.write( <"<td colspan='2'>" + webmaps[rows][columns] + "<p></td>");
+  }
 }
+document.write("</tr>");
+}
+document.write("</table>");
+} ;
